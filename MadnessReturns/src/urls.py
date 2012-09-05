c76eeb7b334cba1a7c35adea94e251ecb9040b96
nba00123 <nba00123@sina.com>
2012-09-05 22:54:29 +0800

    test teamwork

--- MadnessReturns/src/urls.py
+++ MadnessReturns/src/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url, include
 
 from django.contrib import admin
 admin.autodiscover()
-
+#just test
 urlpatterns = patterns('',
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
      url(r'^admin/', include(admin.site.urls)),
