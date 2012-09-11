804b33c062a62aecdf17ea2fe03be2a0d0eec5ff
unknown <ezioruan@.(none)>
2012-09-11 22:08:13 +0800

    delete the dirty dir

---
+++ MadnessReturns/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url, include
+
+from django.contrib import admin
+admin.autodiscover()
+#just test
+urlpatterns = patterns('',
+     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     url(r'^admin/', include(admin.site.urls)),
+     url(r'^$', include('b2b.urls')),
+)
